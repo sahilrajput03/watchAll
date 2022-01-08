81c9bc4319a385ad92a0cc88d5674fe4b2b14714
Sahil Rajput <sahilrajput03@gmail.com>
2022-01-08 15:12:41 +0530

    Massive improvement over unnecessary kill attempt for killing a already killed process, yikes.

--- watchAll.sh
+++ watchAll.sh
@@ -14,11 +14,9 @@ E.g.,
 args=$@ # This is important coz $@ is not accessible in bash functino IDK WHY!
 # My function `actions`:
 actions () {
-  clear               # This is OPTIONAL ~sahil. # TODO: Add a flag to enable/disable it.
-  old_pid=`[ $! ] && ps -aux | grep -v grep | grep -o $!`
-  # FYI:   ^^^^^^^^^ Here I say to only run ps -aux.... command if $! is not empty.
-  echo @INFO:old_pid: $old_pid
-  if [ $old_pid ] ; then kill $!; echo @KILL_INFO: Killed old process with id $!; fi
+  # clear               # This is OPTIONAL ~sahil. # TODO: Add a flag to enable/disable it.
+  old_pid=`[ $! ] && ps -aux | grep -v grep | grep -o $!` # FYI: Here I say to only run ps -aux.... command if $! is not empty.
+  if [ $old_pid ] ; then echo @INFO:old_pid: $old_pid; kill $!; echo @KILL_INFO_1: Killed old process with id $!; fi
   # WORKS SMOOTH :> #if [ $! ] ; then kill $!; echo KILL_INFO: Killed old process with id $!; fi
 
   # echo dollarattherate: $args       # DEBUG ONLY.
@@ -28,7 +26,19 @@ actions () {
   $args &        # works good for ```node myapp.js``` # NOTE THE ENDING & IS ADDED NEWLY ~ SAHIL.
   echo @INFO: $! is the id of the 1.sh process.
   # Checking if the older process is still running or not and only kill it if it does.
-  trap 'if [ $! ] ; then kill $!; echo; echo @KILL_INFO: Killed current process with id $!; fi' exit             # Wow this works so cool... ~ Sahil.
+  # trap - 1 2 3 15 #Clear earlier trap.
+
+    trap '
+      cur_pid=`[ $! ] && ps -aux | grep -v grep | grep -o $!`
+      # Here I check if the subprocess already got killed or not because in case of node myApp.js it gets autokilled so there is no need to kill and throw process not found warning by kill command.
+      # , whereas with ./watchAll bash 1.sh, it does not get killed so we need to kill it manually.
+      if [ $cur_pid ] ; then 
+          if [ $! ] ; then
+          kill $!; echo;
+          echo @KILL_INFO_2: Killed current process with id $!;
+          fi
+      fi
+      ' exit             # Wow this works so cool... ~ Sahil.
 }
 
 actions
